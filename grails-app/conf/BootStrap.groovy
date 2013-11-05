7cdaa2aed515d5298f93fcf30a63302a305543dc
jean-michel guillaume <guillauj@gmail.com>
2013-11-05 22:47:28 +0100

    Overriding behavior of authentication events in BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,13 +1,17 @@
 import org.codehaus.groovy.grails.commons.ApplicationAttributes
 
 import com.grailsrocks.authentication.AuthenticatedUser
+import com.grailsrocks.authentication.AuthenticationService;
 import com.gymadvisor.plugin.authentication.AuthenticationPluginEventsListener
 
 class BootStrap {
-
+	
+	def authenticationService
+	
     def init = { servletContext ->
-		def ctx = servletContext.getAttribute(ApplicationAttributes.APPLICATION_CONTEXT)
-		ctx.addApplicationListener(new AuthenticationPluginEventsListener())
+		authenticationService.events['onSignup'] = { params -> 
+			println "overrided behavior for onSignup"
+		}
     }
     def destroy = {
     }
