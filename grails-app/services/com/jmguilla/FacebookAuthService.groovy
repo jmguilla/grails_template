bb7c1e003c1e3831d04dfb42b93f505702868b6a
jean-michel guillaume <guillauj@gmail.com>
2013-11-17 22:25:24 +0100

    search box put outside of navbar

--- grails-app/services/com/jmguilla/FacebookAuthService.groovy
+++ grails-app/services/com/jmguilla/FacebookAuthService.groovy
@@ -20,6 +20,10 @@ class FacebookAuthService {
   def  Collection<GrantedAuthority> getRoles(User user){
     user.getAuthorities().collect { new GrantedAuthorityImpl(it.authority) } as Set
   }
+  
+  def createRoles(FacebookUser fbUser){
+    //does nothing, hihi
+  }
 
   //TODO centralise user creation
   def User createAppUser(FacebookUser fbUser, FacebookAuthToken token) {
@@ -37,6 +41,8 @@ class FacebookAuthService {
       user.email = fbOAuth.email
       user = user.save(flush: true, failOnError: true)
       UserRole.create(user, Role.findByAuthority('ROLE_USER'), true)
+      UserRole.create(user, Role.findByAuthority('ROLE_FACEBOOK'), true)
+      assert UserRole.findAllByUser(user).size() == 2
       mailService.sendMail {
         to user.email
         subject messageSource.getMessage('user.welcome.subject',[
@@ -45,12 +51,16 @@ class FacebookAuthService {
         body messageSource.getMessage('user.defaultpwd',[password, "http://change.me"].toArray(), 'A new account has been created for you and linked to your Facebook account.' +
         'A default password: $password has also been set. Change it as soon as possible!', new Locale('fr'))}
     }else{
+      //TODO put that after fb user creation
+      int size = UserRole.findAllByUser(user).size()
+      UserRole.create(user, Role.findByAuthority('ROLE_FACEBOOK'), true)
+      assert UserRole.findAllByUser(user).size() == size + 1
       mailService.sendMail {
         to user.email
         subject messageSource.getMessage('user.welcome.subject', [
           user.firstName + " " + user.lastName
         ].toArray(), 'Welcome!', new Locale('fr'))
-        body messageSource.getMessage('user.fblinked'), [].toArray(), 'Your Facebook account has been linked to your existing account.', new Locale('fr')}
+        body messageSource.getMessage('user.fblinked', [].toArray(), 'Your Facebook account has been linked to your existing account.', new Locale('fr'))}
     }
     user
   }
--- grails-app/views/layouts/main.gsp
+++ grails-app/views/layouts/main.gsp
@@ -28,9 +28,49 @@
 						<sec:ifAllGranted roles="ROLE_USER">
 							<sec:username/> (<g:link uri="/j_spring_security_logout">Logout</g:link>)
 						</sec:ifAllGranted>
+						<li>
+							<form class="navbar-form" role="search">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Search">
+							</div>
+							<button type="submit" class="btn btn-default"><g:message code="app.page.landing.search.button.label" default="Submit"/></button>
+							</form>
+						</li>
 					</ul>
 				</div>
 			</div>
+			<nav class="navbar navbar-default" role="navigation">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse"
+					id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+						<li class="dropdown"><a href="#" class="dropdown-toggle"
+							data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li class="divider"></li>
+								<li><a href="#">Separated link</a></li>
+								<li class="divider"></li>
+								<li><a href="#">One more separated link</a></li>
+							</ul></li>
+					</ul>
+				</div>
+				<!-- /.navbar-collapse -->
+			</nav>
 		</div>
 		<g:layoutBody/>
 		<div class="footer" role="contentinfo"></div>
